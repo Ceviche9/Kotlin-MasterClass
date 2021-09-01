658f02fa2a48ecad45ea68194fb23b0e80db9a3f
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-08-31 23:26:57 -0300

    when

--- app/src/main/java/com/example/kotlinudemy/KotlinBasics.kt
+++ app/src/main/java/com/example/kotlinudemy/KotlinBasics.kt
@@ -19,7 +19,55 @@ fun main() {
   // String
   val mySr = "Hello World"
   val firstLetter = mySr[mySr.length - 1]
-  println(firstLetter)
+  // println(firstLetter)
 
-  println("Olá, " + myName)
+  // Operadores aritméticos (+, -, *, /,%)
+
+  // Operadores de comparação (==, !=, > , < , <= , >=)
+
+  // If Statement
+  val heightPerson1 = 170
+  val heightPerson2 = 185
+
+  if (heightPerson1 > heightPerson2) {
+    println("Person 1 is taller then person 2")
+  }else if(heightPerson1 == heightPerson2) {
+    println("the two people are the same height ")
+  } else {
+    println("Person 2 is taller then person 1")
+  }
+
+  // When
+
+  // Criando uma variável com um valor
+  val season = 3
+  when(season) {
+    // Se o valor da variável season for 1 será printado Spring
+    1 -> println("Spring")
+    2 -> println("Summer")
+    3 -> {
+      println("Fall")
+      println("Autumn")
+    }
+    4 -> println("end")
+    else -> println("Invalid season")
+  }
+  // Outro exemplo
+  val month = 3
+  when(month) {
+    // Criando um range
+    in 3..5 -> println("Spring")
+    in 6..8 -> println("Summer")
+    else -> println("Invalid season =")
+  }
+
+  // Outro exemplo
+  val x : Any = 13.37
+  when(x) {
+    is Int -> println("$x is an Int")
+    !is Double -> println("$x is not Double")
+    is String -> println("$x is a String")
+    else -> println("$x is none of the above")
+  }
+
 }
