9f5ac89edf95f6235e4ce35d65ac88a4c2eaeb79
Tundê Cavalcante <ayotunde_sales@hotmail.com>
2021-09-11 17:45:22 -0300

    interface

--- app/src/main/java/com/example/kotlinudemy/Inheritance.kt
+++ app/src/main/java/com/example/kotlinudemy/Inheritance.kt
@@ -1,8 +1,8 @@
 package com.example.kotlinudemy
 
 fun main() {
-  val myCar = NewCar("A3", "Audi")
-  val myECar = ElectricCar("S-Model", "Tesla",85.0)
+  val myCar = NewCar(200.0, "A3", "Audi")
+  val myECar = ElectricCar(200.0, "S-Model","Tesla", 57.9)
 
   myECar.extendRange(200.00)
 
@@ -11,17 +11,32 @@ fun main() {
   // Polymorphism
   myCar.drive(200.00)
   myECar.drive(200.0)
+  // Usando um método da interface
+  myCar.brake()
 }
-
+// A interface funciona como um contrato que a classe tem que seguir
+interface  Drivable {
+  val maxSpeed: Double
+  fun drive(): String
+  // A função brake já é implementada por padão na função
+  fun brake() {
+    println("The drivable is braking")
+  }
+}
 // Para utilizar os métodos e propriedades dessa classe precisamos colocar o "open"
 // NewCar => Super Class
-open class NewCar (val name: String, val brand: String) {
+open class NewCar (override val maxSpeed: Double,
+                   val name: String, val brand: String): Drivable {
   // Para permitir que uma propriedade possa ser sobreescrita.
   open var range: Double = 0.0
 
   fun extendRange(amount: Double) {
     if(amount > 0 )
       range += amount
+  }
+
+  override fun drive(): String {
+    return "Driving the interface drive"
   }
 
   // Para permitir que um método seja sobreescrito
@@ -31,7 +46,7 @@ open class NewCar (val name: String, val brand: String) {
 
 }
 // O carro elétrico pode ter os parâmetros da sua super classe e também pode ter parâmetreos adicionais.
-class ElectricCar(name: String, brand: String, batteryLife: Double): NewCar(name, brand) {
+class ElectricCar(maxSpeed: Double,name: String, brand: String, batteryLife: Double): NewCar(maxSpeed, name, brand) {
 
   //Para sobreescrever algo da Super classe
   override var range = batteryLife * 6
@@ -41,8 +56,8 @@ class ElectricCar(name: String, brand: String, batteryLife: Double): NewCar(name
     println("Drove for $distance KM on electricity")
   }
   // Método que foi criado
-  fun drive() {
-    println("Drove for $range KM on electricity")
+  override fun drive(): String {
+    return "Drove for $range KM on electricity"
   }
 
 }
